6aab6cd63cfc659923f6513adbfcb45bd1940399
dustin-py <dustinstri92@gmail.com>
2021-01-11 07:28:56 -0800

    updated views

--- portfolio/app/views.py
+++ portfolio/app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from database.database import query_column_data
 
-# Create your views here.
+from pdb import set_trace as bp
+
+def home(request):
+    return render(
+        request, 'home.html', {},
+    )
+
+def thm_profile(request):
+    return render(
+        request, 'thm_profile.html', {}
+    )
+
+def thm_badges(request):
+    rows = query_column_data('thm_profile_badges','*')
+    rows = [rows[i] for i in range(len(rows))]
+    return render(
+        request, 'thm_badges.html',
+        {'badge_data':rows})
